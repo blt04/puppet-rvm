bd406caccc21c9c81e8c56c3e6fc56e124681d0b
Avdi Grimm <avdi@avdi.org>
2011-07-15 15:42:58 -0400

    Fixed cat path

--- manifests/definitions/system_user.pp
+++ manifests/definitions/system_user.pp
@@ -7,7 +7,7 @@ define rvm::system_user () {
     }
 
     exec { "/usr/sbin/usermod -a -G $group $username":
-        unless => "/usr/bin/cat /etc/group | grep $group | grep $username",
+        unless => "/bin/cat /etc/group | grep $group | grep $username",
         require => [User[$username], Exec['system-rvm']];
     }
 }
