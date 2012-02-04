03dc6cce5ec56d80a8b812dd714ebb8f578ae97f
Steve Snodgrass <ssnodgra@cmu.edu>
2012-02-03 23:28:29 -0500

    Passenger fixes for selinux on redhat systems

--- manifests/passenger/apache/centos/pre.pp
+++ manifests/passenger/apache/centos/pre.pp
@@ -3,4 +3,20 @@ class rvm::passenger::apache::centos::pre {
   if ! defined(Package['httpd'])       { package { 'httpd':       ensure => installed } }
   if ! defined(Package['httpd-devel']) { package { 'httpd-devel': ensure => installed } }
   if ! defined(Package['mod_ssl'])     { package { 'mod_ssl':     ensure => installed } }
+
+  # Using this temp directory makes passenger work better with selinux
+  file { '/var/run/passenger':
+    ensure => directory,
+    owner  => root,
+    group  => 0,
+    mode   => 0755,
+  }
+
+  if $selinux == 'true' {
+    # Allows apache/passenger to adjust its file descriptor limits
+    selboolean { 'httpd_setrlimit':
+      value      => on,
+      persistent => true,
+    }
+  }
 }
