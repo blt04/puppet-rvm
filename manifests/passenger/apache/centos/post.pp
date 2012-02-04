815c974c2ce1e5f050a192f817bd586e7ebd5c3d
Steve Snodgrass <ssnodgra@cmu.edu>
2012-02-03 23:41:29 -0500

    Make sure apache restarts when passenger config changes under redhat

--- manifests/passenger/apache/centos/post.pp
+++ manifests/passenger/apache/centos/post.pp
@@ -9,18 +9,19 @@ class rvm::passenger::apache::centos::post(
   $rvmpath = $rvm::system::rvmpath,
   $gempath
 ) {
-  exec {
-    'passenger-install-apache2-module':
-      command   => "$rvm_binary ${rvm::passenger::apache::ruby_version} exec passenger-install-apache2-module -a",
-      creates   => "${gempath}/passenger-${version}/ext/apache2/mod_passenger.so",
-      logoutput => 'on_failure',
-      require   => [Rvm_gem['passenger'], Package['httpd','httpd-devel','mod_ssl']];
+  exec { 'passenger-install-apache2-module':
+    command   => "$rvm_binary ${ruby_version} exec passenger-install-apache2-module -a",
+    creates   => "${gempath}/passenger-${version}/ext/apache2/mod_passenger.so",
+    logoutput => 'on_failure',
+    require   => [Rvm_gem['passenger'], Package['httpd','httpd-devel','mod_ssl']];
   }
 
-  file {
-    '/etc/httpd/conf.d/passenger.conf':
-      ensure  => file,
-      content => template('rvm/passenger-apache-centos.conf.erb'),
-      require => Exec['passenger-install-apache2-module'];
+  file { '/etc/httpd/conf.d/passenger.conf':
+    content => template('rvm/passenger-apache-centos.conf.erb'),
+    require => Exec['passenger-install-apache2-module'];
+  }
+
+  if defined(Service['httpd']) {
+    File['/etc/httpd/conf.d/passenger.conf'] ~> Service['httpd']
   }
 }
