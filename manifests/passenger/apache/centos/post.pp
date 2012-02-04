ea11aa8e629101229b7b8556a4f5937aff369370
Steve Snodgrass <ssnodgra@cmu.edu>
2012-02-04 00:27:44 -0500

    Fix selinux contexts for rvm-install passenger files

--- manifests/passenger/apache/centos/post.pp
+++ manifests/passenger/apache/centos/post.pp
@@ -9,11 +9,35 @@ class rvm::passenger::apache::centos::post(
   $rvmpath = $rvm::system::rvmpath,
   $gempath
 ) {
+
+  $passdir = "${gempath}/passenger-${version}"
+
   exec { 'passenger-install-apache2-module':
     command   => "$rvm_binary ${ruby_version} exec passenger-install-apache2-module -a",
-    creates   => "${gempath}/passenger-${version}/ext/apache2/mod_passenger.so",
+    creates   => "$passdir/ext/apache2/mod_passenger.so",
     logoutput => 'on_failure',
     require   => [Rvm_gem['passenger'], Package['httpd','httpd-devel','mod_ssl']];
+  }
+
+  # Fix selinux file contexts
+  # On RHEL6 with a current policy, these are automatic when passenger is
+  # installed in the system ruby gems directory, but the nonstandard rvm
+  # location means they don't get set correctly by the default policy
+  if $selinux == 'true' {
+    file { "$passdir/ext/apache2/mod_passenger.so":
+      seltype => 'lib_t',
+      require => Exec['passenger-install-apache2-module'],
+      before  => File['/etc/httpd/conf.d/passenger.conf'],
+    }
+    file { [
+      "$passdir/agents/PassengerLoggingAgent",
+      "$passdir/agents/PassengerWatchdog",
+      "$passdir/agents/apache2/PassengerHelperAgent"
+    ]:
+      seltype => 'passenger_exec_t',
+      require => Exec['passenger-install-apache2-module'],
+      before  => File['/etc/httpd/conf.d/passenger.conf'],
+    }
   }
 
   file { '/etc/httpd/conf.d/passenger.conf':
