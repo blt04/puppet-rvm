a4b9cd48bd62f7adc801964b5bf00c79eac1de7b
Steve Snodgrass <ssnodgra@cmu.edu>
2012-02-08 12:56:45 -0500

    Parameterize the run stage so it can be changed

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,16 @@
-class rvm($version='latest', $install_rvm=true, $use_rpm=false) {
+class rvm(
+  $version = 'latest',
+  $install_rvm = true,
+  $use_rpm = false,
+  $rstage = 'rvm-install'
+) {
   stage { 'rvm-install': before => Stage['main'] }
 
   if $install_rvm {
     class {
-      'rvm::dependencies': stage => 'rvm-install';
+      'rvm::dependencies': stage => $rstage;
       'rvm::system':
-        stage   => 'rvm-install',
+        stage   => $rstage,
         version => $version,
         use_rpm => $use_rpm;
     }
