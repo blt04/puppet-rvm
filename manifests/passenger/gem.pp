d68924cca3e6c81344f275e41266546de12c7c8d
Steve Snodgrass <ssnodgra@cmu.edu>
2012-02-07 23:43:35 -0500

    Set correct selinux contexts on passenger gem installs

--- manifests/passenger/gem.pp
+++ manifests/passenger/gem.pp
@@ -5,4 +5,11 @@ class rvm::passenger::gem($ruby_version, $version) {
       ruby_version => $ruby_version,
       require      => Rvm_system_ruby["${ruby_version}"],
   }
+  if $selinux == 'true' {
+    exec { 'passenger-contexts':
+      command     => "/sbin/restorecon -R $rvm::system::rvmpath",
+      refreshonly => true,
+      subscribe   => Rvm_gem['passenger'],
+    }
+  }
 }
