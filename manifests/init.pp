df6b1374e32929a6a5a5eee91a3e5990cbdaf944
Steve Snodgrass <ssnodgra@cmu.edu>
2012-02-07 22:58:38 -0500

    Pass the use_rpm param through rvm to rvm::system

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,10 +1,13 @@
-class rvm($version='latest', $install_rvm=true) {
+class rvm($version='latest', $install_rvm=true, $use_rpm=false) {
   stage { 'rvm-install': before => Stage['main'] }
 
   if $install_rvm {
     class {
       'rvm::dependencies': stage => 'rvm-install';
-      'rvm::system':       stage => 'rvm-install', version => $version;
+      'rvm::system':
+        stage   => 'rvm-install',
+        version => $version,
+        use_rpm => $use_rpm;
     }
   }
 }
