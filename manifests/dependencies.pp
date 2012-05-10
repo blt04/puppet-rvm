efc45b9ebba1df3633f47479b1d03e303ef17cc3
Aaron Nichols <anichols@rallydev.com>
2012-05-10 08:36:05 -0600

    Added dependency support for Oracle Linux (tested on 5.7)

--- manifests/dependencies.pp
+++ manifests/dependencies.pp
@@ -2,5 +2,6 @@ class rvm::dependencies {
   case $operatingsystem {
     Ubuntu,Debian: { require rvm::dependencies::ubuntu }
     CentOS,RedHat: { require rvm::dependencies::centos }
+    OracleLinux,RedHat: { require rvm::dependencies::oraclelinux }
   }
 }
