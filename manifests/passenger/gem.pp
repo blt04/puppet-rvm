fc96c63dce4c4af23a2c55e342a72ab5c43a2fa0
Brandon Turner <bt@brandonturner.net>
2012-05-30 11:15:19 -0500

    Fix typo

--- manifests/passenger/gem.pp
+++ manifests/passenger/gem.pp
@@ -1,7 +1,7 @@
 class rvm::passenger::gem($ruby_version, $version) {
   rvm_gem {
     "passenger":
-      ensure => $version,
-      ruby_version => $ruby_version,
+      ensure       => $version,
+      ruby_version => $ruby_version;
   }
 }
