4998f197f5f06bc899ba727f6bccfac463b6967d
andi <a.beuge@betzold.de>
2013-01-09 12:30:50 +0100

    freebsd test

--- manifests/dependencies.pp
+++ manifests/dependencies.pp
@@ -1,5 +1,6 @@
 class rvm::dependencies {
   case $::operatingsystem {
+    FreeBSD: { require rvm::dependencies::freebsd }
     Ubuntu,Debian: { require rvm::dependencies::ubuntu }
     CentOS,RedHat: { require rvm::dependencies::centos }
     OracleLinux,RedHat: { require rvm::dependencies::oraclelinux }
