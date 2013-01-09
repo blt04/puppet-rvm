bd2969eff969f83be250e4241642c4b3ff518390
andi <a.beuge@betzold.de>
2013-01-09 14:53:20 +0100

    freebsd test

--- manifests/dependencies/freebsd.pp
+++ manifests/dependencies/freebsd.pp
@@ -1,28 +1,28 @@
 class rvm::dependencies::freebsd {
 
-  if ! defined(Package['bash'])            { package { 'bash':            ensure => installed } }
-  if ! defined(Package['which'])           { package { 'which':           ensure => installed } }
-  if ! defined(Package['gcc'])             { package { 'gcc':             ensure => installed } }
-  if ! defined(Package['gcc-c++'])         { package { 'gcc-c++':         ensure => installed } }
-  if ! defined(Package['make'])            { package { 'make':            ensure => installed } }
-  if ! defined(Package['gettext-devel'])   { package { 'gettext-devel':   ensure => installed } }
-  if ! defined(Package['expat-devel'])     { package { 'expat-devel':     ensure => installed } }
-  if ! defined(Package['libcurl-devel'])   { package { 'libcurl-devel':   ensure => installed } }
-  if ! defined(Package['zlib-devel'])      { package { 'zlib-devel':      ensure => installed } }
-  if ! defined(Package['openssl-devel'])   { package { 'openssl-devel':   ensure => installed } }
-  if ! defined(Package['perl'])            { package { 'perl':            ensure => installed } }
-  if ! defined(Package['cpio'])            { package { 'cpio':            ensure => installed } }
-  if ! defined(Package['expat-devel'])     { package { 'expat-devel':     ensure => installed } }
-  if ! defined(Package['gettext-devel'])   { package { 'gettext-devel':   ensure => installed } }
-  if ! defined(Package['wget'])            { package { 'wget':            ensure => installed } }
-  if ! defined(Package['bzip2'])           { package { 'bzip2':           ensure => installed } }
-  if ! defined(Package['sendmail'])        { package { 'sendmail':        ensure => installed } }
-  if ! defined(Package['mailx'])           { package { 'mailx':           ensure => installed } }
-  if ! defined(Package['libxml2'])         { package { 'libxml2':         ensure => installed } }
-  if ! defined(Package['libxml2-devel'])   { package { 'libxml2-devel':   ensure => installed } }
-  if ! defined(Package['libxslt'])         { package { 'libxslt':         ensure => installed } }
-  if ! defined(Package['libxslt-devel'])   { package { 'libxslt-devel':   ensure => installed } }
-  if ! defined(Package['readline-devel'])  { package { 'readline-devel':  ensure => installed } }
-  if ! defined(Package['patch'])           { package { 'patch':           ensure => installed } }
-  if ! defined(Package['git'])             { package { 'git':             ensure => installed } }
+  if ! defined(Package['bash'])            { package { 'bash':            ensure => installed, root => true } }
+  if ! defined(Package['which'])           { package { 'which':           ensure => installed, root => true } }
+  if ! defined(Package['gcc'])             { package { 'gcc':             ensure => installed, root => true } }
+  if ! defined(Package['gcc-c++'])         { package { 'gcc-c++':         ensure => installed, root => true } }
+  if ! defined(Package['make'])            { package { 'make':            ensure => installed, root => true } }
+  if ! defined(Package['gettext-devel'])   { package { 'gettext-devel':   ensure => installed, root => true } }
+  if ! defined(Package['expat-devel'])     { package { 'expat-devel':     ensure => installed, root => true } }
+  if ! defined(Package['libcurl-devel'])   { package { 'libcurl-devel':   ensure => installed, root => true } }
+  if ! defined(Package['zlib-devel'])      { package { 'zlib-devel':      ensure => installed, root => true } }
+  if ! defined(Package['openssl-devel'])   { package { 'openssl-devel':   ensure => installed, root => true } }
+  if ! defined(Package['perl'])            { package { 'perl':            ensure => installed, root => true } }
+  if ! defined(Package['cpio'])            { package { 'cpio':            ensure => installed, root => true } }
+  if ! defined(Package['expat-devel'])     { package { 'expat-devel':     ensure => installed, root => true } }
+  if ! defined(Package['gettext-devel'])   { package { 'gettext-devel':   ensure => installed, root => true } }
+  if ! defined(Package['wget'])            { package { 'wget':            ensure => installed, root => true } }
+  if ! defined(Package['bzip2'])           { package { 'bzip2':           ensure => installed, root => true } }
+  if ! defined(Package['sendmail'])        { package { 'sendmail':        ensure => installed, root => true } }
+  if ! defined(Package['mailx'])           { package { 'mailx':           ensure => installed, root => true } }
+  if ! defined(Package['libxml2'])         { package { 'libxml2':         ensure => installed, root => true } }
+  if ! defined(Package['libxml2-devel'])   { package { 'libxml2-devel':   ensure => installed, root => true } }
+  if ! defined(Package['libxslt'])         { package { 'libxslt':         ensure => installed, root => true } }
+  if ! defined(Package['libxslt-devel'])   { package { 'libxslt-devel':   ensure => installed, root => true } }
+  if ! defined(Package['readline-devel'])  { package { 'readline-devel':  ensure => installed, root => true } }
+  if ! defined(Package['patch'])           { package { 'patch':           ensure => installed, root => true } }
+  if ! defined(Package['git'])             { package { 'git':             ensure => installed, root => true } }
 }
