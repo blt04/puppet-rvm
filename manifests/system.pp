4b64ce30dd083d2e2121db7b93d14126d64f008c
andi <a.beuge@betzold.de>
2013-01-09 16:44:19 +0100

    freebsd systemwide rvm but from the 'rails' user

--- manifests/system.pp
+++ manifests/system.pp
@@ -1,14 +1,13 @@
 class rvm::system($version='latest') {
   exec { 'system-rvm':
     path    => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
-    command => "sudo bash -c 'curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer -o /tmp/rvm-installer ; \
+    command => "login; sudo bash -c 'curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer -o /tmp/rvm-installer ; \
                 chmod +x /tmp/rvm-installer ; \
                 rvm_bin_path=/usr/local/rvm/bin rvm_man_path=/usr/local/rvm/man /tmp/rvm-installer --version ${version} ; \
                 rm /tmp/rvm-installer'",
     creates => '/usr/local/rvm/bin/rvm',
     require => [
       Class['rvm::dependencies'],
-    ],
-    user => rails
+    ]
   }
 }
