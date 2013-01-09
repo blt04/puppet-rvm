94c027c24ab498cdd61c889b9faf612177597235
andi <a.beuge@betzold.de>
2013-01-09 15:09:48 +0100

    freebsd test

--- manifests/system.pp
+++ manifests/system.pp
@@ -1,12 +1,11 @@
 class rvm::system($version='latest') {
   exec { 'system-rvm':
-    path    => '/usr/bin:/usr/sbin:/bin',
+    path    => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
     command => "bash -c '/usr/bin/curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer -o /tmp/rvm-installer ; \
                 chmod +x /tmp/rvm-installer ; \
                 rvm_bin_path=/usr/local/rvm/bin rvm_man_path=/usr/local/rvm/man /tmp/rvm-installer --version ${version} ; \
                 rm /tmp/rvm-installer'",
     creates => '/usr/local/rvm/bin/rvm',
-    provider => shell,
     require => [
       Class['rvm::dependencies'],
     ],
