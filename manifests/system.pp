f3b2991755f9028f244551d096a2373fca59b236
Brett Porter <brett@maestrodev.com>
2013-06-04 17:52:18 -0700

    only attempt to upgrade if RVM is already installed
    Previously, if you set a version or latest, then both the initial install and
    the upgrade would attempt to run, with the latter failing due to the notify
    resource being missing. Moved the upgrade into the check that $::rvm_version
    was set

--- manifests/system.pp
+++ manifests/system.pp
@@ -21,18 +21,18 @@ class rvm::system($version=undef) {
   # the fact won't work until rvm is installed before puppet starts
   if "${::rvm_version}" != "" {
     notify { 'rvm_version': message => "RVM version ${::rvm_version}" }
-  }
 
-  if ($version != undef) and ($version != present) and ($version != $::rvm_version) {
-    # Update the rvm installation to the version specified
-    notify { 'rvm-get_version':
-      message => "RVM updating to version ${version}",
-      require => Notify['rvm_version'],
-    } ->
-    exec { 'system-rvm-get':
-      path    => '/usr/local/rvm/bin:/usr/bin:/usr/sbin:/bin',
-      command => "rvm get ${version}",
-      before  => Exec['system-rvm'], # so it doesn't run after being installed the first time
+    if ($version != undef) and ($version != present) and ($version != $::rvm_version) {
+      # Update the rvm installation to the version specified
+      notify { 'rvm-get_version':
+        message => "RVM updating to version ${version}",
+        require => Notify['rvm_version'],
+      } ->
+      exec { 'system-rvm-get':
+        path    => '/usr/local/rvm/bin:/usr/bin:/usr/sbin:/bin',
+        command => "rvm get ${version}",
+        before  => Exec['system-rvm'], # so it doesn't run after being installed the first time
+      }
     }
   }
 
