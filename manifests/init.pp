12195359d10b5ce783ae9664c31eb7a1d047eed2
Joshua Rutherford <joshua.rutherford@opower.com>
2013-10-14 13:32:43 -0700

    Allow installation from mirrored repositories
    RVM's installer now supports the specification of a source repository
    for installation from non standard locations (i.e., internally hosted
    repositories).

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,10 +1,23 @@
-class rvm($version=undef, $install_rvm=true) {
+class rvm(
+  $version = undef,
+  $install_rvm = true,
+  $url = 'https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer',
+  $source = 'github.com/wayneeseguin/rvm') {
+
   stage { 'rvm-install': before => Stage['main'] }
 
   if $install_rvm {
-    class {
-      'rvm::dependencies': stage => 'rvm-install';
-      'rvm::system':       stage => 'rvm-install', version => $version;
+
+    class {'rvm::dependencies':
+      stage => 'rvm-install'
     }
+
+    class {'rvm::system':
+      stage => 'rvm-install',
+      version => $version,
+      url => $url,
+      source  => $source;
+    }
+
   }
 }
