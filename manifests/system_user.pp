64b79d69d7bd8699af308f3b1aa1fd3d201d4ee0
Carlos Sanchez <csanchez@maestrodev.com>
2014-08-18 20:08:24 +0200

    Formatting and lint

--- manifests/system_user.pp
+++ manifests/system_user.pp
@@ -1,21 +1,19 @@
 define rvm::system_user (
-  $create=true ) {
+  $create = true) {
 
   include rvm::params
 
   if $create {
-
-  ensure_resource('user', $name, {'ensure' => 'present' })
-
+    ensure_resource('user', $name, {'ensure' => 'present' })
   }
 
   include rvm::group
 
-  $add_to_group = $osfamily ? {
+  $add_to_group = $::osfamily ? {
     'Darwin' => "/usr/sbin/dseditgroup -o edit -a ${name} -t user ${rvm::params::group}",
     default  => "/usr/sbin/usermod -a -G ${rvm::params::group} ${name}",
   }
-  $check_in_group = $osfamily ? {
+  $check_in_group = $::osfamily ? {
     'Darwin' => "/usr/bin/dsmemberutil checkmembership -U ${name} -G ${rvm::params::group} | grep -q 'user is a member'",
     default  => "/bin/cat /etc/group | grep '^${rvm::params::group}:' | grep -qw ${name}",
   }
