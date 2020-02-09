ba786b2ce62dce98a8d9db8746c09206491a267c
Alexander Fisher <alex@linfratech.co.uk>
2020-02-09 15:36:25 +0000

    Rubocop: Fix Style/RedundantReturn

--- lib/puppet/provider/rvm_gem/gem.rb
+++ lib/puppet/provider/rvm_gem/gem.rb
@@ -59,12 +59,12 @@ Puppet::Type.type(:rvm_gem).provide(:gem) do
   def self.gemsplit(desc)
     case desc
     when /^\*\*\*/, /^\s*$/, /^\s+/; return nil
-    when /gem: not found/; return nil
+    when /gem: not found/; nil
     # when /^(\S+)\s+\((((((\d+[.]?))+)(,\s)*)+)\)/
     when /^(\S+)\s+\((\d+.*)\)/
       name = $1
       version = $2.split(/,\s*/)
-      return {
+      {
         name: name,
         ensure: version
       }
