d2902454c61a7341d1a91df6332bfadc4a9fafd3
Alexander Fisher <alex@linfratech.co.uk>
2020-02-09 15:54:52 +0000

    Rubocop: Fix Style/RedundantBegin

--- lib/puppet/provider/rvm_system_ruby/rvm_system_ruby.rb
+++ lib/puppet/provider/rvm_system_ruby/rvm_system_ruby.rb
@@ -19,23 +19,19 @@ Puppet::Type.type(:rvm_system_ruby).provide(:rvm) do
   end
 
   def exists?
-    begin
-      rvmcmd('list', 'strings').split("\n").any? do |line|
-        line =~ Regexp.new(Regexp.escape(resource[:name]))
-      end
-    rescue Puppet::ExecutionFailure => detail
-      raise Puppet::Error, "Could not list RVMs: #{detail}"
+    rvmcmd('list', 'strings').split("\n").any? do |line|
+      line =~ Regexp.new(Regexp.escape(resource[:name]))
     end
+  rescue Puppet::ExecutionFailure => detail
+    raise Puppet::Error, "Could not list RVMs: #{detail}"
   end
 
   def default_use
-    begin
-      rvmcmd('list', 'default').split("\n").any? do |line|
-        line =~ Regexp.new(Regexp.escape(resource[:name]))
-      end
-    rescue Puppet::ExecutionFailure => detail
-      raise Puppet::Error, "Could not list default RVM: #{detail}"
+    rvmcmd('list', 'default').split("\n").any? do |line|
+      line =~ Regexp.new(Regexp.escape(resource[:name]))
     end
+  rescue Puppet::ExecutionFailure => detail
+    raise Puppet::Error, "Could not list default RVM: #{detail}"
   end
 
   def default_use=(value)
